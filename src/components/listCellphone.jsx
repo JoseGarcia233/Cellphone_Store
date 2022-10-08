cbe64e06660d8ad085b07a2f9ab2f54ae9465f5d
JoseGarcia233 <viguera27@gmail.com>
2022-10-08 18:12:23 -0400

    1.1

--- src/components/listCellphone.jsx
+++ src/components/listCellphone.jsx
@@ -1,27 +1,25 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import React from 'react';
+import '../css/listcellp.css';
 
 export default function ListCellphone() {
     const style1 = {width:'18rem' }
     const img4 = 'https://fdn2.gsmarena.com/vv/pics/motorola/motorola-edge30-neo-1.jpg';
   return (
-    <div class="card" style={style1}>
-  <img class="card-img-top" src="https://specs-tech.com/wp-content/uploads/2022/08/Motorola-Edge-30-Neo-4.jpg" alt="Card image cap" /> 
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className="contanerC">
+    <div className="card" >
+
+        <img className="card-img-top" src="https://fdn2.gsmarena.com/vv/pics/motorola/motorola-edge30-neo-1.jpg" alt="Card image cap" /> 
+   
+  <div className="card-body">
+    <h6 className="card-title"> <b>Brand:</b> motorola </h6>
+    <h6 className="card-text"><b>Model:</b> G4</h6>
+    <h6 className="card-text"><b>Storage:</b> 32gb</h6>
+    <h6 className="card-text"><b>Price:</b> 10000</h6>
     <a href="#/" class="btn btn-primary">Go somewhere</a>
   </div>
-  <div className="ncard">
-  <div class="card" style={style1}>
-  <img class="card-img-top" src="https://fdn2.gsmarena.com/vv/pics/motorola/motorola-edge30-neo-1.jpg" alt="Card image cap" /> 
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#/" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  </div>
+ 
 </div>
+  </div>
   )
 }
